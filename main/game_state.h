609388f5e8013165c83cb395a967cf25cbb805a2
emgilfuster <emgilfuster@gmail.com>
2020-10-09 01:59:49 +0200

    adding start level function

--- main/game_state.h
+++ main/game_state.h
@@ -65,3 +65,93 @@ vector<double> polar_statevector(vector<std::complex<double>> cartesian) {
 	return polar_statevector;
 }
 
+vector<vector<vector<double>>> start_level(int level) {
+	// gives initial state and goal state for each level
+	// returns states, with states[0] initial state and state[1] goal state
+
+	// initialize variables
+	vector<vector<vector<double>>> states;
+	vector<vector<double>> initial, goal;
+
+	switch (lvl)	// Initial and goal states are hard coded for each level
+	{				// This ain't pretty
+	case 0:
+		for (int i = 0; i < 8; i++) {
+			vector<double> polar1, polar2;
+			if (i == 0) {
+				polar1.push_back(1.0);
+			}
+			else {
+				polar1.push_back(0.0);
+			}
+			polar1.push_back(0.0);
+
+			if (i == 6) {
+				polar2.push_back(1.0);
+				polar2.push_back(M_PI);
+			}
+			else {
+				polar2.push_back(0.0);
+				polar2.push_back(0.0);
+			}
+			initial.push_back(polar1);
+			goal.push_back(polar2);
+		}
+
+	case 1:
+		aux = 1 / sqrt(2);
+		for (int i = 0; i < 8; i++) {
+			vector<double> polar1, polar2;
+			polar1.push_back(0.5 * aux);
+			polar1.push_back(0.0);
+
+			if (i == 6) {
+				polar2.push_back(aux);
+				polar2.push_back(0.0);
+			}
+			else if (i == 7) {
+				polar2.push_back(aux);
+				polar2.push_back(M_PI)M
+			}
+			else {
+				polar2.push_back(0.0);
+				polar2.push_back(0.0);
+			}
+			initial.push_back(polar1);
+			goal.push_back(polar2);
+		}
+
+	case 2:
+		aux = 1 / sqrt(2);
+		for (int i = 0; i < 8; i++) {
+			vector<double> polar1, polar2;
+			if (i == 0 || i == 3) {
+				polar1.push_back(aux);
+			}
+			else {
+				polar1.push_back(0.0);
+			}
+			polar1.push_back(0.0);
+
+			if (i == 3) {
+				polar2.push_back(aux);
+				polar2.push_back(-0.5 * M_PI);
+			}
+			else if (i == 5) {
+				polar2.push_back(aux);
+				polar2.push_back(0.5 * M_PI);
+			}
+			else {
+				polar2.push_back(0.0);
+				polar2.push_back(0.0);
+			}
+			initial.push_back(polar1);
+			goal.push_back(polar2);
+		}
+	}
+
+	states.push_back(initial);
+	states.push_back(goal);
+
+	return states;
+}

f75382bd88239eb3a410c50d4bc734755f97d228
Nick-Singstock <35930611+Nick-Singstock@users.noreply.github.com>
2020-10-08 22:55:00 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -82,8 +82,8 @@ int main() {
 
 	vector<std::complex<double>> goal_comp = gm.goal_state_complex();
 	cout << "Goal State:" << endl;
-	for (int j = 0; j < goal.size(); j++) {
-		cout << goal[j] << endl;
+	for (int j = 0; j < goal_comp.size(); j++) {
+		cout << goal_comp[j] << endl;
 	}
 
 
